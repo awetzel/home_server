3a1bf187986a7e6560eb4348b37022f0077f294f
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2020-09-09 13:16:50 +0200

    first conf

--- lib/home_server.ex
+++ lib/home_server.ex
@@ -1,18 +1,13 @@
 defmodule HomeServer do
-  @moduledoc """
-  Documentation for HomeServer.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HomeServer.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  use Application
+  def start(_,_) do
+    Supervisor.start_link([
+        {DHCPServer, ["enp0s3", [
+           gateway: "192.168.0.254",
+           netmask: "255.255.0.0",
+           range: {"192.168.1.1", "192.168.9.254"},
+           domain_servers: ["192.168.0.254"]
+         ]]}
+      ], strategy: :one_for_one, name: __MODULE__)
   end
 end
