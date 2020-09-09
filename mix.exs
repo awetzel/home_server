78b7cc530419e7cf3a1cba6ed950c122610c23a4
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2020-09-09 13:02:06 +0200

    first commit

---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule HomeServer.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :home_server,
+      version: "0.1.0",
+      elixir: "~> 1.9",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:dhcp_server, "~> 0.7.0"}
+    ]
+  end
+end
